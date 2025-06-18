394b086a7e8017881f2241562d49e18a0054d12b
[tomoya] <[is0778hx@ed.ritsumei.ac.jp]>
2025-06-18 14:42:14 +0900

    sunRise time YYMMDDHHMM

---
+++ getSunRise.js
@@ -0,0 +1,19 @@
+function getDailySunrise() {
+  const LAT = 34.8163; // 茨木市の緯度
+  const LNG = 135.5685; // 茨木市の経度
+  const url = `https://api.sunrise-sunset.org/json?lat=${LAT}&lng=${LNG}&formatted=0`;
+
+  const response = UrlFetchApp.fetch(url);
+  const data = JSON.parse(response.getContentText());
+
+  const sunriseUTC = new Date(data.results.sunrise);
+  const sunriseJST = new Date(sunriseUTC.getTime() + 9 * 60 * 60 * 1000);
+
+  const formatted = formatDate(sunriseJST);
+  Logger.log('日の出（10桁）: ' + formatted);
+  return formatted;
+}
+
+function formatDate(date) {
+  return Utilities.formatDate(date, 'Asia/Tokyo', 'yyMMddHHmm');
+}
