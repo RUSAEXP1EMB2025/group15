aee603d86843e91cb9b0457ee71cd86bab65921f
2eru <is0803pe@ed.ritsumei.ac.jp>
2025-07-09 12:05:38 +0900

    change dir , upload weekly-document,final pptx

--- document/spreadsheet_link.md
+++
@@ -1,2 +0,0 @@
-# link
-https://docs.google.com/spreadsheets/d/1Bq_cCBmQ-TU5bbZ1Ca7_0zKhvSBq3i9N7GM-0Z8zQuU/edit?gid=0#gid=0
---
+++ src/line/stopAlarm.js
@@ -0,0 +1,14 @@
+function doPost(e) {
+  const json = JSON.parse(e.postData.contents);
+
+  // メッセージイベントか確認
+  if (json.events && json.events[0].type === 'message') {
+    const props = PropertiesService.getScriptProperties();
+    props.setProperty('stopSending', 'true'); // フラグを立てる
+    Logger.log('ユーザーからメッセージを受信。送信停止フラグを設定しました。');
+    return ContentService.createTextOutput(JSON.stringify({ status: 'ok' })).setMimeType(ContentService.MimeType.JSON);
+  }
+
+  
+}
+
